9de075ef223cae9afb519db25483a168a17d746c
Gleydson Menezes <gsmenezes996@gmail.com>
2024-02-22 11:54:46 -0300

    #2 Triangle - done

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 import entities.Product;
+import entities.Triangle;
 
 public class Program {
 
@@ -12,21 +13,21 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		
-		System.out.println("Enter the number of exercícies do you'll see: "
-				+ "1 - Product"
-				+ "2 - Triangle"
-				+ "3 - Rectangle"
-				+ "4 - Employee"
-				+ "5 - Student"
+		System.out.println("Enter the number of exercícies do you'll see: \n"
+				+ "1 - Product\n"
+				+ "2 - Triangle\n"
+				+ "3 - Rectangle\n"
+				+ "4 - Employee\n"
+				+ "5 - Student\n"
 				+ "0 - Exit");
 		int number = sc.nextInt();
+		sc.nextLine();
 		
 		switch(number) {
 			
 		case 1:
 			
 			Product product = new Product();
-			
 			System.out.println("Enter product data: ");
 			System.out.print("Name: ");
 			product.name = sc.nextLine();
@@ -58,6 +59,32 @@ public class Program {
 			break;
 		
 		case 2:
+			Triangle x, y;
+			x  = new Triangle();
+			y  = new Triangle();
+			
+			System.out.println("Enter the measures of triangle X: ");
+			x.a = sc.nextDouble();
+			x.b = sc.nextDouble();
+			x.c = sc.nextDouble();
+			System.out.println("Enter the measures of triangle Y: ");
+			y.a = sc.nextDouble();
+			y.b = sc.nextDouble();
+			y.c = sc.nextDouble();
+			
+			System.out.printf("Triangle x area: %.4f%n", x.area());
+			System.out.printf("Triangle y area: %.4f%n", y.area());
+			
+			if (x.area() > y.area()) {
+				System.out.println("Larger area: X");
+			} 
+			else {
+				System.out.println("Larger area: Y");
+			}
+			break;
+			
+		case 3:
+			
 			
 		}
 		
